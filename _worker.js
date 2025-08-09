191bb3b1cbbb8afd7e4290777ebe412bafc1eb6f
dapei0402 <76612589+dapei0402@users.noreply.github.com>
2025-08-09 13:53:18 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -13,7 +13,7 @@ let timestamp = 4102329600000;//2099-12-31
 
 //节点链接 + 订阅链接
 let MainData = `
-https://shz.al/~86-tg@pgkj0402
+https://shz.al/~89-tg@pgkj0402
 `
 
 let urls = [];
@@ -831,5 +831,6 @@ async function KV(request, env, txt = 'ADD.txt', guest) {
 		});
 	}
 }
+
 
 
